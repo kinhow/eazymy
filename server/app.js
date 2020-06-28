eb48b955fc88cb5695b70470d83141def5513095
kinhow <kinhow_93@hotmail.com>
2020-06-28 11:20:25 +0800

    updated changes

--- server/app.js
+++ server/app.js
@@ -36,12 +36,10 @@ app.use(function (err, req, res, next) {
   res.render('error');
 });
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+app.use(express.static(path.join(__dirname, '/client/build')));
 
-  app.get('*', (req, res) => {
-    res.send(express.static(path.join(__dirname, '../client/build/index.html')));
-  });
-}
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/client/build/index.html'));
+});
 
 module.exports = app;
